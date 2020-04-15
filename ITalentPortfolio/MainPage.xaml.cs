f1cb4522d108738f7cb4ed925a0004dd38a6fffd
Warre Dreesen <dreesen.warre@gmail.com>
2020-04-15 13:30:01 +0200

    initial commit

---
+++ ITalentPortfolio/MainPage.xaml.cs
@@ -0,0 +1,92 @@
+﻿using ITalentPortfolio.Pages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media.Animation;
+using Windows.UI.Xaml.Navigation;
+
+namespace ITalentPortfolio
+{
+    /// <summary>
+    /// The main page onto which the navigation and pages are loaded.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            NavView.SelectedItem = NavView.MenuItems.ElementAt(0);
+            ContentFrame.Navigate(typeof(HomePage));
+        }
+
+        private void ContentFrame_NavigationFailed(object sender, NavigationFailedEventArgs e)
+        {
+            DisplayCannotNavigateDialog();
+        }
+
+        private readonly List<(string Tag, Type Page)> _pages = new List<(string Tag, Type Page)>
+        {
+            ("home", typeof(HomePage)),
+            ("hackathon", typeof(HackathonPage)),
+            ("seminar", typeof(SeminarPage)),
+            ("trip", typeof(TripPage)),
+            ("studentjob", typeof(StudentJobPage))
+        };
+
+
+        private void NavView_ItemInvoked(NavigationView sender,
+                                         NavigationViewItemInvokedEventArgs args)
+        {
+            if (args.InvokedItemContainer != null)
+            {
+                var navItemTag = args.InvokedItemContainer.Tag.ToString();
+                NavView_Navigate(navItemTag, args.RecommendedNavigationTransitionInfo);
+            }
+        }
+
+        private void NavView_Navigate(string navItemTag, NavigationTransitionInfo transitionInfo)
+        {
+            Type _page = null;
+
+            var item = _pages.FirstOrDefault(p => p.Tag.Equals(navItemTag));
+            _page = item.Page;
+
+            var preNavPageType = ContentFrame.CurrentSourcePageType;
+
+            if (!(_page is null) && !Type.Equals(preNavPageType, _page))
+            {
+                ContentFrame.Navigate(_page, null, transitionInfo);
+            }
+        }
+
+        private void On_Navigated(object sender, NavigationEventArgs e)
+        {
+            NavView.IsBackEnabled = ContentFrame.CanGoBack;
+
+            if (ContentFrame.SourcePageType != null)
+            {
+                var item = _pages.FirstOrDefault(p => p.Page == e.SourcePageType);
+
+                NavView.SelectedItem = NavView.MenuItems
+                    .OfType<NavigationViewItem>()
+                    .First(n => n.Tag.Equals(item.Tag));
+
+                NavView.Header =
+                    ((NavigationViewItem)NavView.SelectedItem)?.Content?.ToString();
+            }
+        }
+
+        private async void DisplayCannotNavigateDialog()
+        {
+            ContentDialog cannotNavigate = new ContentDialog()
+            {
+                Title = "Page not found",
+                Content = "Could not navigate to the requested page. Please try again.",
+                CloseButtonText = "Ok"
+            };
+
+            await cannotNavigate.ShowAsync();
+        }
+    }
+}
